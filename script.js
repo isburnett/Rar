b987f810783cf589766e83aa5f1035beb8b57711
isburnett <ian.sol.burnett@gmail.com>
2025-03-28 17:51:08 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('proxyForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const url = document.getElementById('url').value;
-    const proxyUrl = `/proxy/${encodeURIComponent(url)}`;
-    document.getElementById('proxyIframe').src = proxyUrl;
-});
